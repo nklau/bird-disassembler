e9e205df553e376e7b4fa4d5372c2eb9434b6cda
nklau2016@gmail.com <63122247+nklau@users.noreply.github.com>
2024-04-22 16:10:07 -0700

    explicit parentheses in output

--- src/disassembler.js
+++ src/disassembler.js
@@ -4,16 +4,17 @@ export const disassemble = (input, output = '') => {
   const symbols = input.match(/\(\S+\)|[WCRFV]\*\*|[IWCRFV]\*|[BDQW]1|[BDMQ]2|[BQ]3|Q4|[B-M]|[Q-W]|[OYÊ]/g)
 
   symbols.forEach(symbol => {
+    // TODO probably ignores **
     if (symbol === core.S.combinator || symbol === core.K.combinator) {
       output += symbol
     } else if (symbol.startsWith('(')) {
       if (/^\([SK]+\)$/.test(symbol)) {
-        output += symbol
+        output = `(${output}${symbol})`
       } else {
-        output += disassemble(symbol.slice(1, symbol.length - 1), output)
+        output = `(${disassemble(symbol.slice(1, symbol.length - 1), output)})`
       }
     } else {
-      output += disassemble(core[symbol].combinator, output)
+      output = `(${disassemble(core[symbol].combinator, output)})`
     }
   })
 
